c8e1eac788c2df478eeb097aea553a08a823ae3a
Alan Willoughby <satyadvaya@gmail.com>
2020-04-03 04:04:19 -0700

    full QUnit pass

--- test/check-result.test.js
+++ test/check-result.test.js
@@ -6,6 +6,47 @@ import { checkResult } from '../check-result.js';
 const test = QUnit.test;
 
 test('time to test a function', function(assert) {
+
+    //Arrange
+    // Set up your parameters and expectations
+
+    //Act 
+    // Call the function you're testing and set the result to a const
+    
+    const userThrow = 'rock';
+    const computerThrow = 'rock';
+    const expected = 'draws';
+    const outcome = checkResult(userThrow, computerThrow);
+
+    //Assert
+    // Make assertions about what is expected valid result
+
+    assert.equal(outcome, expected);
+    
+});
+
+test('time to test a function', function(assert) {
+
+    //Arrange
+    // Set up your parameters and expectations
+
+    //Act 
+    // Call the function you're testing and set the result to a const
+    
+    const userThrow = 'rock';
+    const computerThrow = 'paper';
+    const expected = 'loses';
+    const outcome = checkResult(userThrow, computerThrow);
+
+    //Assert
+    // Make assertions about what is expected valid result
+
+    assert.equal(outcome, expected);
+    
+});
+
+test('time to test a function', function(assert) {
+
     //Arrange
     // Set up your parameters and expectations
 
@@ -15,18 +56,131 @@ test('time to test a function', function(assert) {
     const userThrow = 'rock';
     const computerThrow = 'scissors';
     const expected = 'wins';
-    const throwOutcome = checkResult(userThrow, computerThrow);
-
-    // const wins = checkResult('rock', 'scisors');
-    // const draws = checkResult('rock', 'rock');
-    // const loses = checkResult('rock', 'scissors');
+    const outcome = checkResult(userThrow, computerThrow);
 
     //Assert
     // Make assertions about what is expected valid result
+
+    assert.equal(outcome, expected);
     
-    assert.equal(throwOutcome, expected);
+});
+
+test('time to test a function', function(assert) {
+
+    //Arrange
+    // Set up your parameters and expectations
+
+    //Act 
+    // Call the function you're testing and set the result to a const
     
-    // assert.equal(wins, true);
-    // assert.equal(draws, true);
-    // assert.equal(loses, true);
+    const userThrow = 'paper';
+    const computerThrow = 'rock';
+    const expected = 'wins';
+    const outcome = checkResult(userThrow, computerThrow);
+
+    //Assert
+    // Make assertions about what is expected valid result
+
+    assert.equal(outcome, expected);
+    
+});
+
+test('time to test a function', function(assert) {
+
+    //Arrange
+    // Set up your parameters and expectations
+
+    //Act 
+    // Call the function you're testing and set the result to a const
+    
+    const userThrow = 'paper';
+    const computerThrow = 'paper';
+    const expected = 'draws';
+    const outcome = checkResult(userThrow, computerThrow);
+
+    //Assert
+    // Make assertions about what is expected valid result
+
+    assert.equal(outcome, expected);
+    
+});
+
+test('time to test a function', function(assert) {
+
+    //Arrange
+    // Set up your parameters and expectations
+
+    //Act 
+    // Call the function you're testing and set the result to a const
+    
+    const userThrow = 'paper';
+    const computerThrow = 'scissors';
+    const expected = 'loses';
+    const outcome = checkResult(userThrow, computerThrow);
+
+    //Assert
+    // Make assertions about what is expected valid result
+
+    assert.equal(outcome, expected);
+    
+});
+
+test('time to test a function', function(assert) {
+
+    //Arrange
+    // Set up your parameters and expectations
+
+    //Act 
+    // Call the function you're testing and set the result to a const
+    
+    const userThrow = 'scissors';
+    const computerThrow = 'rock';
+    const expected = 'loses';
+    const outcome = checkResult(userThrow, computerThrow);
+
+    //Assert
+    // Make assertions about what is expected valid result
+
+    assert.equal(outcome, expected);
+    
+});
+
+test('time to test a function', function(assert) {
+
+    //Arrange
+    // Set up your parameters and expectations
+
+    //Act 
+    // Call the function you're testing and set the result to a const
+    
+    const userThrow = 'scissors';
+    const computerThrow = 'paper';
+    const expected = 'wins';
+    const outcome = checkResult(userThrow, computerThrow);
+
+    //Assert
+    // Make assertions about what is expected valid result
+
+    assert.equal(outcome, expected);
+    
+});
+
+test('time to test a function', function(assert) {
+
+    //Arrange
+    // Set up your parameters and expectations
+
+    //Act 
+    // Call the function you're testing and set the result to a const
+    
+    const userThrow = 'scissors';
+    const computerThrow = 'scissors';
+    const expected = 'draws';
+    const outcome = checkResult(userThrow, computerThrow);
+    
+    //Assert
+    // Make assertions about what is expected valid result
+
+    assert.equal(outcome, expected);
+    
 });
